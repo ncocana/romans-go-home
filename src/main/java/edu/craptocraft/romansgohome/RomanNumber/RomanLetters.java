36a598586a46bea3a4785040a451f28620a6b2a8
ncocana <ncocana@cifpfbmoll.eu>
2023-03-08 19:25:55 +0100

    feat(RomanLetters): add enum class for the roman letters

---
+++ src/main/java/edu/craptocraft/romansgohome/RomanNumber/RomanLetters.java
@@ -0,0 +1,20 @@
+package edu.craptocraft.romansgohome.RomanNumber;
+
+public enum RomanLetters {
+    
+    I(1), V(5), X(10), L(50),
+    C(100), D(500), M(1000),
+	IV(4), IX(9), XL(40),
+    XC(90),CD(400), CM(900);
+
+    private int valueDecimal;
+
+    private RomanLetters(int valueDecimal) {
+        this.valueDecimal = valueDecimal;
+    }
+
+    public int getValueDecimal() {
+        return this.valueDecimal;
+    }
+
+}

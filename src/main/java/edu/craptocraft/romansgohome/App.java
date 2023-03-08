6baf1f00618887966d750370602bbfbb0bf4617e
ncocana <ncocana@cifpfbmoll.eu>
2023-03-08 16:40:30 +0100

    feat(all): add pom, gitignore, main app, and basic test

---
+++ src/main/java/edu/craptocraft/romansgohome/App.java
@@ -0,0 +1,17 @@
+package edu.craptocraft.romansgohome;
+
+import java.util.List;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List.of( "MMMDCCCLXXXVIII", // 3888
+				 "MMDCCLXXVII",  // 2777
+				 "CDXLIV", // 444
+				 "CDXXXIX" // 439
+		).stream()
+			.map(RomanNumber::new)
+			.forEach(n -> System.out.println(n.toString() + " = " + n.toDecimal()));
+    }
+}
